8be4063bc3be18093c22b01237ad3e7b12b34a1b
Vivek Gundu <62993315+Vivek0624@users.noreply.github.com>
2022-09-19 22:47:03 +0530

    initial commit
    my initial commit for react-crud

---
+++ precache-manifest.6eab5df6499e47279f7a54d997d0b51f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee9524d0b5278786e0654d587074aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d65685e3d019d1f42c",
+    "url": "/static/css/2.b94af48b.chunk.css"
+  },
+  {
+    "revision": "69e5b5c6a7289369426d",
+    "url": "/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "28d65685e3d019d1f42c",
+    "url": "/static/js/2.59bfa3ef.chunk.js"
+  },
+  {
+    "revision": "69f55f7331ecbe5c6f344e6aa8b3760d",
+    "url": "/static/js/2.59bfa3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e5b5c6a7289369426d",
+    "url": "/static/js/main.0c68f721.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0c68f721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fba879f4a0a836c42ba",
+    "url": "/static/js/runtime-main.6eeaf4fc.js"
+  }
+]);
